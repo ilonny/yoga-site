2ca740a5148416f619ac0ae7e36848114a86de64
Dmitry Home <di@goldcarrot.ru>
2018-03-02 22:32:33 +0500

    start-create-categories

--- views/site/about.php
+++ views/site/about.php
@@ -4,15 +4,12 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'О компании';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        Здесь текст о сайте
     </p>
-
-    <code><?= __FILE__ ?></code>
 </div>
